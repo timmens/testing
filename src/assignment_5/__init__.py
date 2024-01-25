42caf544f43cf5eb5430035a78743f60288ce011
timmens <mensingertim@gmail.com>
2024-01-25 08:40:30 +0100

    'Initial commit.'

---
+++ src/assignment_5/__init__.py
@@ -0,0 +1,1 @@
+"""Code for producing the results of the Assignment 5 project."""
---
+++ src/assignment_5/final/__init__.py
@@ -0,0 +1,5 @@
+"""Functions for formatting results."""
+
+from assignment_5.final.plot import plot_regression_by_age
+
+__all__ = [plot_regression_by_age]
